b07a2b9814452e0d180e14af2b1fbbcaf7e6b621
hemanth p <hemanth9103@gmail.com>
2025-01-13 20:14:18 +0530

    minor changes

--- src/main/java/com/hemanth/simplewebApp/controller/LoginController.java
+++ src/main/java/com/hemanth/simplewebApp/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.hemanth.simplewebApp;
+package com.hemanth.simplewebApp.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
