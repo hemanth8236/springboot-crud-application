a15204278f3138427c87a0de26de5c9a1a1ef879
hemanth p <hemanth9103@gmail.com>
2025-01-13 20:21:06 +0530

    minor changes

--- src/main/java/com/hemanth/simplewebApp/controller/HomeController.java
+++ src/main/java/com/hemanth/simplewebApp/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.hemanth.simplewebApp;
+package com.hemanth.simplewebApp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
